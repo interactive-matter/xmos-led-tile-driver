ce0d0797eb5dd137f4772e8871fb374880ab24e7
Marcus Nowotny <marcus@interactive-matter.eu>
2011-09-15 19:12:52 +0200

    this is where the drawing test should go

---
+++ src/test/java/TestDisplayData.java
@@ -0,0 +1,27 @@
+import eu.interactivematter.xmosledtiledriver.XMOSLedTileDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A test class to test different display data.
+ * <p/>
+ * <p/>
+ * Copyright (c) 2011, Interactive Matter, Marcus Nowotny, All rights reserved
+ * This software is freely distributable under a derivative of the
+ * University of Illinois/NCSA Open Source License posted in
+ * LICENSE.txt and at <http://github.xcore.com/>
+ */
+public class TestDisplayData {
+
+  private XMOSLedTileDriver driver;
+
+  @Before
+  public void setup() {
+    driver = new XMOSLedTileDriver();
+  }
+
+  @Test
+  public void sendSimplePatterns() {
+
+  }
+}
