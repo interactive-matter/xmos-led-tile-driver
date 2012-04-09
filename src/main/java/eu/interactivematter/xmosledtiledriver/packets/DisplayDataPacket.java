fbcb98a3813bea2d5820bd089700619ed8c7add8
Marcus Nowotny <marcus@interactive-matter.eu>
2012-04-09 18:19:52 +0200

    we need little endian format for the payload

--- src/main/java/eu/interactivematter/xmosledtiledriver/packets/DisplayDataPacket.java
+++ src/main/java/eu/interactivematter/xmosledtiledriver/packets/DisplayDataPacket.java
@@ -6,6 +6,7 @@ import eu.interactivematter.xmosledtiledriver.XMOSLedTilePacketPayload;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * <p/>
@@ -32,23 +33,18 @@ public class DisplayDataPacket extends XMOSLedTilePacketPayload {
 
   @Override
   public byte[] getPayloadAsBytes() {
-    try {
-      DataOutputStream stream = getDataStream();
-      //we encode 0 as width & height
-      stream.writeShort(0); //the tile width
-      stream.writeShort(0); //the tile height
-      //the next two positions are reserved
-      stream.write(new byte[]{0, 0});
-      //next write out the pixel pointer
-      stream.writeShort(pixelPointer);
-      stream.writeShort(pixelData.length);
-      //the next two positions are reserved
-      stream.write(new byte[]{0, 0});
-      stream.write(pixelData);
-    }
-    catch (IOException e) {
-      throw new XMOSLedTileDriverException("Unable to write to UDP data stream", e);
-    }
+    ByteBuffer payloadBuffer = getPayloadBuffer();
+    //we encode 0 as width & height
+    payloadBuffer.putShort((short) 0); //the tile width
+    payloadBuffer.putShort((short) 0); //the tile height
+    //the next two positions are reserved
+    payloadBuffer.put(new byte[]{0, 0});
+    //next write out the pixel pointer
+    payloadBuffer.putShort(pixelPointer);
+    payloadBuffer.putShort((short) pixelData.length);
+    //the next two positions are reserved
+    payloadBuffer.put(new byte[]{0, 0});
+    payloadBuffer.put(pixelData);
     return super.getPayloadAsBytes();
   }
 }
